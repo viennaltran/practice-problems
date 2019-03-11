0faef6faeed467724b84da92a9a0e4b057cf275f
viennaltran <lanvienhtran@gmail.com>
2019-03-10 23:09:52 -0700

    fixed a syntax

--- learningfuze/array_sort.js
+++ learningfuze/array_sort.js
@@ -11,8 +11,8 @@
 var myArray = ['mouse', 'cat', 'dog', 'human'];
 
 
-function sortString(str){
-  var arr = myArray;
+function sortString(arr){
+  arr = myArray;
   var tmp;
   for(var i = 0; i < arr.length; i++){
     for(var j = i + 1; j < arr.length; j++){
