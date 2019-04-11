15ee87a45460b36ee59fd8db48f408912c76cfb5
viennaltran <lanvienhtran@gmail.com>
2019-04-10 20:34:55 -0700

    level2 completed

--- you_can't_javascript/level2.js
+++ you_can't_javascript/level2.js
@@ -1,5 +1,10 @@
 function isNumberEven(i) {  
     // i will be an integer. Return true if it's even, and false if it isn't. 
-    
+    if(i%2===0){
+        return true;
+    } 
+    else if (i%2===1){
+        return false;
+    }
     
 }
