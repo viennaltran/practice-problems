4d1834ec0ea5602a74a223871e14d04d145fc613
viennaltran <lanvienhtran@gmail.com>
2019-04-25 17:48:31 -0700

    completed fibonacci sequence problem

--- learningfuze/numeric_spiral.js
+++ learningfuze/numeric_spiral.js
@@ -6,3 +6,35 @@
 // Example:
 // fibSequence(12);
 // Output - 0 1 1 2 3 5 8 13 21 34 55 89
+
+
+var output = [];
+function fibonacci (number){
+  if (number>3){
+    var i =0;
+    output.push(i);
+    output.push(i+1);
+  
+  for (i=0; i<number-2; i++){
+    output.push(output[i]+output[i+1])
+  }
+  return output
+  }
+  else if (number === 2){
+    var j =0;
+    output.push(j);
+    output.push(j+1);
+    return output
+  }
+  else if (number === 1){
+    var k =0;
+    output.push(k);
+    return output
+  }
+  else if (number === 0 ){
+    console.log("no sequence"); 
+  }else{
+    console.log("not a whole number");
+  }
+}
+console.log(fibonacci(12))
