3b4e030cb91d593c4de9606368ca75c77468f0cf
viennaltran <lanvienhtran@gmail.com>
2019-05-29 16:59:41 -0700

    simple frizzbuzz solution

---
+++ frizzBuzz.js
@@ -0,0 +1,16 @@
+function fizzy(n) {
+    if (n % 15 === 0) {
+      return "FizzBuzz";
+    }
+    if (n % 3 === 0) {
+      return "Fizz";
+    }
+    if (n % 5 === 0) {
+      return "Buzz";
+    }
+    return n.toString();
+  }
+  
+  for (let i = 1; i <= 100; i++) {
+    console.log(fizzy(i));
+  }
