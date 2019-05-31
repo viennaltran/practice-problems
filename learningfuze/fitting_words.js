b96b3d1bf1c0366e6d69f76b6d555bc9a7d3351e
viennaltran <lanvienhtran@gmail.com>
2019-05-30 21:07:21 -0700

    fixed the code so it would work for any word fitting

--- learningfuze/fitting_words.js
+++ learningfuze/fitting_words.js
@@ -1,32 +1,28 @@
 // Build a function that takes in a string and an array of strings. The function should output an 
 // array of strings that contain the same letters as the single input string.
 
-var myString= "cat";
 var myArray = ['cat','caterpillar','whale','accurate','smile','cactus','cute'];
-var newArray = [];
-var newArray2 = [];
-var newArray3 = [];
+var arrayOfMatchingWords = [];
 
-function fittingWords (string,array){
-    var letter=string.split("");
-    
-    for(i=0; i<array.length; i++){
-    
-    if(myArray[i].includes(letter[0])){
-      newArray.push(array[i]);
-      
-      if(myArray[i].includes(letter[1])){
-        newArray2.push(array[i]);
-        
-        if(myArray[i].includes(letter[2])){
-          newArray3.push(array[i]);
-        }
-      }
-    }else{
-      console.log(false);
-    }
-   }
-      
-    return newArray3
+ function fittingWords(string, arrayOfStrings){
+  
+    for(var i = 0; i < arrayOfStrings.length; i++){
+       if(myArray[i].includes(string[0])){
+         arrayOfMatchingWords.push(myArray[i])
+         
+           for(var x = 0; x < string.length; x++){
+             
+             for(var y = 0; y < arrayOfMatchingWords.length; y++){
+         
+          if(!arrayOfMatchingWords[y].includes(string[x])){
+            arrayOfMatchingWords.splice(y,1); 
+
+          }       
+       }    
+    } 
   }
-  console.log(fittingWords(myString, myArray));
+}
+return arrayOfMatchingWords;
+}
+
+fittingWords('cat', myArray);
